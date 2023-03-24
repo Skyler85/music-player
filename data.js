722506c16af8edf955658ad6608adf795eacd493
Skyler <worstlie1@gmail.com>
2023-03-24 22:00:03 +0900

    file Update
    mp3 and cover image

---
+++ data.js
@@ -0,0 +1,37 @@
+let songs = [
+  {
+    name: "Only U(Original)",
+    path: "musics/song 1.mp3",
+    artist: "Moon Sujin & Jiselle",
+    cover: "images/cover 1.jpg",
+    // Moon Sujin & Jiselle(지젤) - Only U (Original ver. ) Official Lyric Video
+  },
+  {
+    name: "old school love",
+    path: "musics/song 2.mp3",
+    artist: "leejean",
+    cover: "images/cover 2.jpg",
+    // leejean - old school love
+  },
+  {
+    name: "Make the Move",
+    path: "musics/song 3.mp3",
+    artist: "Soovi(feat. pH-1)",
+    cover: "images/cover 3.jpg",
+    //수비 (Soovi) - Make the Move (feat. pH-1) [Official Video]
+  },
+  {
+    name: "Way U Are",
+    path: "musics/song 4.mp3",
+    artist: "Jiselle(feat. BIG Naughty)",
+    cover: "images/cover 4.jpg",
+    // 지셀 (Jiselle) 'Way U Are (feat. BIG Naughty)' Lyric Video
+  },
+  {
+    name: "song 5",
+    path: "musics/song 5.mp3",
+    artist: "artist 5",
+    cover: "images/cover 5.jpg",
+    // moon sujun - never let me go
+  },
+];
